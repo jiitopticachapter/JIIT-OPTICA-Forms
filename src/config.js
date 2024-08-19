e22d99c5309acf587462727e8a3a85a5acc166d6
Harsh Sharma <harshsharma20503@gmail.com>
2024-08-19 22:44:53 +0530

    initial commit

---
+++ src/config.js
@@ -0,0 +1,11 @@
+const formDetails = {
+  formHeading: {
+    heading: "Form Heading",
+    font: "Arial",
+  },
+  formDescription: {
+    description: "Form Description",
+    font: "Arial",
+  },
+  formInputs: [],
+};
