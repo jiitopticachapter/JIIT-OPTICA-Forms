1d37ab2bbd2980cc1fb7983fc3ed251e33757be6
Rajatt09 <rajatbhati9559@gmail.com>
2024-11-06 16:38:55 +0530

    checking data

--- src/pages/Designs/FormDesign3.jsx
+++ src/pages/Designs/FormDesign3.jsx
@@ -23,7 +23,7 @@ import optica_logo from "./../../assets/optica_logo.png";
 import { FaTwitter, FaLinkedin, FaInstagram, FaGithub } from "react-icons/fa";
 
 // import Data from "../../Components/DataStructure/Data";
-import LatentData from "../../Components/DataStructure/Data";
+import LatentData from "../../Components/DataStructure/Data.js";
 import HandleSubmit from "../../Components/DataStructure/HandleSubmit";
 import useError from "../../Components/DataStructure/ValidateError";
 // import { Errors } from "../../Components/DataStructure/ValidateError";
