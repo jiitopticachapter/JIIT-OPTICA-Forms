682b6e9acf5a36f7a45805adf01ca1228cdbc4c1
Dishi_Gautam <mail2dishig@gmail.com>
2025-01-17 20:01:28 +0530

    deadline page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ConfirmPage from "./pages/ConfirmPage/ConfirmPage.jsx";
+import DeadLine from "./pages/DeadLinePage/DeadLinePage.jsx";
 import JaypeeGotLatent from "./pages/Designs/JaypeeGotLatentDesign.jsx";
 import { Toaster } from "react-hot-toast";
 import MacOSDesign from "./pages/Designs/MacOSDesign.jsx";
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<MacOSDesign />} />
         <Route path="/form-submitted" element={<ConfirmPage />} />
+        <Route path="/deadline-passed" element={<DeadLine/>}/>
       </Routes>
 
       <Toaster />
